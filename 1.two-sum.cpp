faee0803c1ec45d17bcbea4919777dcda1b2f197
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-08-30 00:06:07 +0530

    finding two elements whose sum equal to target

---
+++ 1.two-sum.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=1 lang=cpp
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // vector<int> v;
+        // for(int i=0; i<nums.size(); i++){
+        //     for(int j=i+1; j<nums.size(); j++){
+        //         if((nums[i]+nums[j])==target){
+        //             v.push_back(i);
+        //             v.push_back(j);
+        //             break;
+        //         }
+        //     }
+        // }
+        // return v;
+
+        vector<int> mp, v;
+        mp = nums;
+        int count = 0;
+    
+        for(int i=nums.size()-1; i>=0; i--){
+            int num = target-nums[i];
+            for(int j=nums.size()-1; j>=0; j--){
+                if(mp[j]==num && j!=i){
+                    v.push_back(j);
+                    count++;
+                }
+            }
+            if(count==2)
+                break;
+        }
+        return v;
+    }
+};
+// @lc code=end
+
