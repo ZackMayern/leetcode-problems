46d9c31bfb3473b10b3ac19ecb3f18d4d238e14f
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-08-31 20:58:41 +0530

    solved two sums

---
+++ 15.3-sum.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=15 lang=cpp
+ *
+ * [15] 3Sum
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> finalResult;
+        vector<int> result;
+        int leftPointer, midPointer = leftPointer+1, rightPointer = midPointer+1;
+
+        return finalResult;
+    }
+};
+// @lc code=end
+
