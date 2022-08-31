c47c5f6bdf31e6e7d5df08c84886cd19f21dfbb4
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-08-31 23:25:40 +0530

    solved three sum

--- 15.3-sum.cpp
+++ 15.3-sum.cpp
@@ -8,11 +8,37 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> finalResult;
-        vector<int> result;
-        int leftPointer, midPointer = leftPointer+1, rightPointer = midPointer+1;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
 
-        return finalResult;
+        for(int index=0; index<nums.size()-2; index++){
+            if(index == 0 || (index > 0 && nums[index]!=nums[index-1])){
+                int leftPointer=index+1, rightPointer=nums.size()-1;
+                int sum = -nums[index];
+
+                while(leftPointer<rightPointer){
+                    if(nums[leftPointer]+nums[rightPointer]==sum){
+                        vector<int> temp;
+                        temp.push_back(nums[index]);
+                        temp.push_back(nums[leftPointer]);
+                        temp.push_back(nums[rightPointer]);
+                        result.push_back(temp);
+
+                        while(leftPointer<rightPointer && nums[leftPointer]==nums[leftPointer+1]) leftPointer++;
+                        while(leftPointer<rightPointer && nums[rightPointer]==nums[rightPointer-1]) rightPointer--;
+
+                        leftPointer++; 
+                        rightPointer--;
+                    }
+                    else if(nums[leftPointer]+nums[rightPointer]>sum)
+                        rightPointer--;
+
+                    else
+                        leftPointer++;
+                } 
+            }
+        }
+        return result;
     }
 };
 // @lc code=end
