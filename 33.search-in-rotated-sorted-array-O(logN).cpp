839c5b3f8a63d7bd26f6ee307b8e74a8e166618b
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-09-01 23:50:41 +0530

    added comments

--- 33.search-in-rotated-sorted-array-O(logN).cpp
+++ 33.search-in-rotated-sorted-array-O(logN).cpp
@@ -25,30 +25,30 @@ public:
         
         while(start<=end){
             int mid = (start+end)/2;
-            if(mid < end && nums[mid]>nums[mid+1])
+            if(mid<end && nums[mid]>nums[mid+1]) // Finding the largest element in the array and return the pivot position 
                 return mid;
-            if(mid > start && nums[mid]<nums[mid-1])
+            if(mid>start && nums[mid]<nums[mid-1]) 
                 return mid-1;
-            else if(nums[mid] < nums[start])
+            else if(nums[mid]<nums[start]) // if neither, if the mid value is less than start, end is shifted to the previous of mid position.
                 end = mid-1;
-            else
+            else  // if neither, if the mid value is greater than start, start is shifted to the next of mid position.
                 start = mid+1;
         }
-        return -1;
+        return -1;   // Return -1 if pivot couldn't be found.
     }
 
     int search(vector<int>& nums, int target) {
         int n = nums.size()-1;
-        int pivot = findPivot(nums);
+        int pivot = findPivot(nums);  // Finding the pivot using binary search
 
         if(pivot==-1)
-            return binarySearch(nums, target, 0, n);
-        if(nums[pivot]==target)
+            return binarySearch(nums, target, 0, n);  // If pivot couldn't be found the array is probably too short.
+        if(nums[pivot]==target)   // If by chance pivot element is same as the target, then return pivot.
             return pivot;
-        if(target>=nums[0])
+        if(target>=nums[0])  // If target is larger than the first index of the rotated array, probably it lies between the starting and the pivot position.
             return binarySearch(nums, target, 0, pivot-1);
 
-        return binarySearch(nums, target, pivot+1, n);
+        return binarySearch(nums, target, pivot+1, n);  // If target is lesser than the first index of the rotated array, probably it lies between the next to pivot and the last elements.
     }
 };
 // @lc code=end
