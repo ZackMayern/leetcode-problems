154ce1d4c57f58690975abae03b458b387a6824e
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-09-02 23:13:48 +0530

    added another binary search approach

--- 74.search-a-2-d-matrix.cpp
+++ 74.search-a-2-d-matrix.cpp
@@ -7,6 +7,7 @@
 // @lc code=start
 class Solution {
 public:
+    // Storing elements in an array and performing binary search on the vector.
     // bool binarySearch(vector<int> ele, int tar){
     //     int start = 0, end = ele.size()-1;
     //     while(start<=end){
@@ -38,26 +39,44 @@ public:
     //     return flag;
     // }
 
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        vector<int> elements;
-        bool flag = false;
+    // Using Binary search in the matrix itself.
+    // bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    //     vector<int> elements;
+    //     bool flag = false;
         
-        for(int i=0; i<matrix.size(); i++){
-            // Traversing the row and implementing Binary Search in columns.
-            int start = 0, end = matrix[0].size()-1;
-            while(start<=end){
-                int mid = (start+end)/2;
-                if(matrix[i][mid]==target)
-                    return true;
-                else{
-                    if(matrix[i][mid]>target)
-                        end = mid-1;
-                    else
-                        start = mid+1;
-                }
-            }
+    //     for(int i=0; i<matrix.size(); i++){
+    //         // Traversing the row and implementing Binary Search in columns.
+    //         int start = 0, end = matrix[0].size()-1;
+    //         while(start<=end){
+    //             int mid = (start+end)/2;
+    //             if(matrix[i][mid]==target)
+    //                 return true;
+    //             else{
+    //                 if(matrix[i][mid]>target)
+    //                     end = mid-1;
+    //                 else
+    //                     start = mid+1;
+    //             }
+    //         }
+    //     }
+    //     return flag;
+    // }
+
+    // Binary Search approach but using a formula.
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size(), n = matrix[0].size();
+        int start = 0, end = (m*n)-1;
+
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(matrix[mid/n][mid%n]==target)
+                return true;
+            else if(matrix[mid/n][mid%n]>target)
+                end = mid-1;
+            else
+                start = mid+1;
         }
-        return flag;
+        return false;
     }
 };
 // @lc code=end
