8d18600910c62d0aaee0afee51939195b4fac9e0
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-09-03 00:47:42 +0530

    finding if the given sudoku is valid or not

--- 36.valid-sudoku.cpp
+++ 36.valid-sudoku.cpp
@@ -7,8 +7,27 @@
 // @lc code=start
 class Solution {
 public:
+    bool validCheck(vector<vector<char>>& board, int row, int column){
+        if(board[row][column]=='.')
+            return true;
+        for(int i=0; i<9; i++){
+            if(board[row][column]==board[i][column] && i!=row)
+                return false;
+            if(board[row][column]==board[row][i] && i!=column)
+                return false;
+            if(board[row][column]==board[row-row%3+i/3][column-column%3+i%3] && row%3!=i/3 && column%3!=i%3)
+                return false;
+        }
+        return true;
+    }
     bool isValidSudoku(vector<vector<char>>& board) {
-        
+        for(int row=0; row<9; row++){
+            for(int col=0; col<9; col++){
+                if(!validCheck(board, row, col))
+                    return false;
+            }
+        }
+        return true;
     }
 };
 // @lc code=end
