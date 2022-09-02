f7235c2350179fa1fc7dfb4ad29b5d1fb92ed47b
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-09-02 15:32:48 +0530

    added DP approach

--- 5.longest-palindromic-substring.cpp
+++ 5.longest-palindromic-substring.cpp
@@ -12,7 +12,7 @@ public:
         int maxLength = 1;
         int start = 0, end = 0;
         
-        if(s.length()<=1)
+        if(n<=1)
             return s;
 
         //For odd palindrome string
@@ -53,6 +53,39 @@ public:
             }
         }
         return s.substr(start, maxLength);
+
+        // DP Approach
+        // int n = s.size();
+        // int maxLength = 0;
+        // string answer;
+        // vector<vector<int>> matrix(n,vector<int>(n,0));
+        
+        // for(int diff=0; diff<n; diff++){
+        //     for(int i=0, j=i+diff; j<n; i++, j++){
+        //         if(i==j)
+        //             matrix[i][j]=1;
+
+        //         else if(diff==1){
+        //             if(s[i]==s[j])
+        //                 matrix[i][j]=2;
+        //             else
+        //                 matrix[i][j]=0;
+        //         }
+
+        //         else{
+        //             if(s[i]==s[j] && matrix[i+1][j-1])
+        //                 matrix[i][j]=matrix[i+1][j-1]+2;
+        //         }
+
+        //         if(matrix[i][j]){
+        //             if(j-i+1>maxLength){
+        //                 maxLength = j-i+1;
+        //                 answer = s.substr(i,maxLength);
+        //             }
+        //         }
+        //     }
+        // }
+        // return answer;
     }
 };
 // @lc code=end
