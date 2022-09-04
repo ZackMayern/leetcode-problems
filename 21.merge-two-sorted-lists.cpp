3688ff4c4e7dab6544d99f636b8b260a6f16b24a
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-09-04 22:13:02 +0530

    merging two sorted linked lists using a dummy node

---
+++ 21.merge-two-sorted-lists.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=21 lang=cpp
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        struct ListNode *resultList = new ListNode();
+        ListNode *tail = resultList;
+        
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                tail->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                tail->next = list2;
+                list2 = list2->next;
+            }
+            tail = tail->next;
+        }
+
+        if(list1)
+            tail->next = list1;
+        else if(list2)
+            tail->next = list2;
+
+        return resultList->next;
+    }
+};
+// @lc code=end
+
