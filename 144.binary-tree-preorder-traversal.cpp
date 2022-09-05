c0df7fd4625064fe9d5b72986e238d926664ba2c
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-09-05 17:46:27 +0530

    added recursion approach

--- 144.binary-tree-preorder-traversal.cpp
+++ 144.binary-tree-preorder-traversal.cpp
@@ -18,21 +18,34 @@
  */
 class Solution {
 public:
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     vector<int> result;
+    //     if(root==NULL)
+    //         return result;
+    //     stack<TreeNode*> s;
+    //     s.push(root);
+    //     while(!s.empty()){
+    //         TreeNode* node = s.top();
+    //         s.pop();
+    //         result.push_back(node->val);
+    //         if(node->right)
+    //             s.push(node->right);
+    //         if(node->left)
+    //             s.push(node->left);
+    //     }
+    //     return result;
+    // }
+    void preOrder(TreeNode* root, vector<int>& a){
+        if(root==NULL)
+            return;
+        
+        a.push_back(root->val);
+        preOrder(root->left, a);
+        preOrder(root->right, a);
+    }
     vector<int> preorderTraversal(TreeNode* root) {
         vector<int> result;
-        if(root==NULL)
-            return result;
-        stack<TreeNode*> s;
-        s.push(root);
-        while(!s.empty()){
-            TreeNode* node = s.top();
-            s.pop();
-            result.push_back(node->val);
-            if(node->right)
-                s.push(node->right);
-            if(node->left)
-                s.push(node->left);
-        }
+        preOrder(root, result);
         return result;
     }
 };
