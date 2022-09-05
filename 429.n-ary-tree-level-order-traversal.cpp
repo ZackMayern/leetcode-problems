c3ef18786eb28030a2d9d303db2961cdd4c68651
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-09-06 00:07:48 +0530

    level order traversal of an n-ary tree

---
+++ 429.n-ary-tree-level-order-traversal.cpp
@@ -0,0 +1,52 @@
+/*
+ * @lc app=leetcode id=429 lang=cpp
+ *
+ * [429] N-ary Tree Level Order Traversal
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(Node* root) {
+        vector<vector<int>> result;
+        queue<Node*> q;
+        if(root == NULL)
+            return result;
+        q.push(root);
+        while(!q.empty()){
+            int n = q.size();
+            vector<int> temp;
+            for(int i=0; i<n; i++){
+                Node* node = q.front();
+                q.pop();
+                for(Node* val:node->children)
+                    q.push(val);
+                temp.push_back(node->val);
+            }
+            result.push_back(temp);
+        }
+        return result;
+    }
+};
+// @lc code=end
+
