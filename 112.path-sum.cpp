51cc29a9f5d8bb14be5563b4136dd00069436a9f
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-09-24 20:00:06 +0530

    solved day 12 of DS chapter

---
+++ 112.path-sum.cpp
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode id=112 lang=cpp
+ *
+ * [112] Path Sum
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if(root==NULL)
+            return false;
+        if(root->left==NULL && root->right==NULL){
+            if(targetSum-root->val==0)
+                return true;
+            else
+                return false;
+        }
+        return (hasPathSum(root->left, targetSum-root->val) || hasPathSum(root->right, targetSum-root->val));
+    }
+};
+// @lc code=end
+
