d83220c93938c0919a6492b29681120c03f96a22
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-10-02 21:36:06 +0530

    solved biweekly easy question

---
+++ Biweekly Contest/88/removeLetterToEqualizeFrequency.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool equalFrequency(string word){
+    vector<int> freq(26,0);
+
+    for(auto x:word)
+        freq[x-'a']++;
+    
+    for(int i=0; i<26; i++){
+        vector<int> temp=freq;
+        if(temp[i]<=0)
+            continue;
+
+        temp[i]--;
+        set<int> st;
+
+        for(auto it:temp){
+            if(it>0)
+                st.insert(it);
+        }
+
+        if(st.size()==1)
+            return true;
+    }
+    return false;
+}
+
+int main(){
+    string word;
+    cin >> word;
+
+    bool result=equalFrequency(word);
+
+    if(result)
+        cout << "true";
+    else
+        cout << "false";
+}
