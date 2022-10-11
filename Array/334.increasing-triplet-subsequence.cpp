c2a2fb5f5430d4612746bbb73635781f172b1086
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-10-11 22:11:23 +0530

    Solved a medium array question

---
+++ Array/334.increasing-triplet-subsequence.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=334 lang=cpp
+ *
+ * [334] Increasing Triplet Subsequence
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        if(nums.size()<3)
+            return false;
+        int left = INT_MAX;
+        int mid = INT_MAX;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]>mid)
+                return true;
+            else if(nums[i]>left && nums[i]<mid)
+                mid=nums[i];
+            else if(nums[i]<left)
+                left=nums[i];
+        }
+        return false;
+    }
+};
+// @lc code=end
+
