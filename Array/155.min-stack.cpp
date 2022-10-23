7224fbefb474b753b3341d9e4d6f47dc966b40ee
Anubhav Sahu <anubhavsahu71@gmail.com>
2022-10-24 02:13:15 +0530

    Solved a medium question on designing stacks

---
+++ Array/155.min-stack.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=155 lang=cpp
+ *
+ * [155] Min Stack
+ */
+
+// @lc code=start
+class MinStack {
+public:
+    vector<int>v;
+    MinStack() {
+         
+    }
+    
+    void push(int val) {
+        v.push_back(val);
+    }
+    
+    void pop() {
+        v.pop_back();
+    }
+    
+    int top() {
+        return v[v.size()-1];
+    }
+    
+    int getMin() {
+        return *min_element(v.begin(),v.end());
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
+// @lc code=end
+
